de00bac81964afae028d53b1d97f152bf99d528c
beeth0ven <beeth0vendev@gmail.com>
2018-02-21 00:13:27 +0800

    Add feature syncComputerIPWithRecordIP

---
+++ server/config.js
@@ -0,0 +1,11 @@
+const region = 'ap-northeast-2';
+const hostedZoneId = 'Z1SQZP97745SVF';
+const recordName = 'home.beeth0ven.cf';
+const updateInterval = 10 * 60 * 1000;
+
+export {
+  region,
+  hostedZoneId,
+  recordName,
+  updateInterval
+}
---
+++ server/index.js
@@ -0,0 +1,29 @@
+import PublicIP from 'public-ip';
+import {getRecordIP, updateRecordIP} from "./awsService";
+import {updateInterval} from "./config";
+
+const syncIPIfNeeded = async () => {
+  console.log('\nsyncIPIfNeeded start at:', new Date());
+  try {
+    const computerPublicIP = await PublicIP.v4();
+    const recordIP = await getRecordIP();
+    console.log('computerPublicIP', computerPublicIP);
+    console.log('recordIP', recordIP);
+    if (computerPublicIP === recordIP) {
+      console.log('No need to update record IP');
+      return;
+    }
+    await updateRecordIP(computerPublicIP);
+    console.log('Update Record IP Success:', computerPublicIP);
+  } catch (error) {
+    console.error('Update Record IP Fail:', error);
+  } finally {
+    console.log('syncIPIfNeeded end at:', new Date());
+  }
+};
+
+setInterval(async () => {
+  await syncIPIfNeeded();
+}, updateInterval);
+
+console.log('it works!');
